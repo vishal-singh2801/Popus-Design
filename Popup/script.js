a78dcf52e083f1b6762bbbf8bdd45ac63dd43d83
Vishal Singh <72906722+vishal-singh2801@users.noreply.github.com>
2024-02-05 19:11:26 +0530

    Add files via upload
     we will create a model box or Popup website using HTML, CSS, and JavaScript. We will add some animation to this popup using CSS transitions.

---
+++ Popup/script.js
@@ -0,0 +1,9 @@
+let popus = document.getElementById("popus");
+
+function openPopus(){
+    popus.classList.add("open-popus");  
+}
+
+function closePopus(){
+    popus.classList.remove("open-popus");  
+}
